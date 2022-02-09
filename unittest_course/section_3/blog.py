c741fea45720e4b6746cd320e1eda68ab85c0640
JanoBourian <supermath33@icloud.com>
2022-02-08 20:03:14 -0600

    Add: new test

--- unittest_course/section_3/blog.py
+++ unittest_course/section_3/blog.py
@@ -5,15 +5,15 @@ class Blog:
         self.title = title
         self.author = author
         self.posts = []
-    
+
+    def __repr__(self):
+        return f"{self.title} by {self.author} with {len(self.posts)} posts"
+
     def create_post(self, title: str, content: str): 
         p = Post(title = title, content = content)
         self.posts.append(p)
         return p 
         
-    
     def json(self):
         return {"title": self.title, "author": self.author, "posts": self.posts} 
     
-    def __repr__(self):
-        pass 
--- unittest_course/section_3/tests/unit/test_blog.py
+++ unittest_course/section_3/tests/unit/test_blog.py
@@ -10,3 +10,16 @@ class BlogTestClass(TestCase):
         self.assertEqual(author, b.author)
         self.assertListEqual([], b.posts)
         self.assertEqual(len([]), len(b.posts))
+    
+    def test_repr_method_with_zero_posts(self):
+        title = "Blog random"
+        author = "Django Rendhairt"
+        b = Blog(title=title, author=author)
+        self.assertEqual(b.__repr__(), f"{title} by {author} with {len(b.posts)} posts")
+    
+    def test_repr_method_with_some_posts(self):
+        title = "Blog random"
+        author = "Django Rendhairt"
+        b = Blog(title=title, author=author)
+        b.posts = ["One", "Two", "Three", "Four", "Five"]
+        self.assertEqual(b.__repr__(), f"{title} by {author} with {len(b.posts)} posts")
